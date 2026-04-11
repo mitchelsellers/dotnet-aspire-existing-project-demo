56d2a204cf719715415f2b47149014efdf2fb082
Mitchel Sellers <msellers@iowacomputergurus.com>
2026-04-10 22:52:33 -0500

    Baseline Demo Project Setup for Discussions

---
+++ src/SampleRazorpages/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleRazorpages.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
